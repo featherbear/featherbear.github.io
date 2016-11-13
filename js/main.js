fc9d59649d401ec126b6c916a634e095fa62e812
Andrew Wong <andrew12345@live.com>
2016-11-13 19:13:53 +1100

    KYCK again!

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
 /* Custom stuff */
 
 var imgMin = 1;
-var imgMax = 67;
+var imgMax = 69;
 Math.randomRange = function(min, max) {
 	// When random isn't random enough - http://heyjavascript.com/improving-on-javascripts-random-numbers/		
 	if (min && max) {
