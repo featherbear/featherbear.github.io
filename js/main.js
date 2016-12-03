7c15e074ffb20e151962cd430afbaf26e0b2133f
Andrew Wong <andrew12345@live.com>
2016-12-03 21:40:58 +1100

    Matt Zhang's 21th!

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
 /* Custom stuff */
 
 var imgMin = 1;
-var imgMax = 87;
+var imgMax = 88;
 Math.randomRange = function(min, max) {
 	// When random isn't random enough - http://heyjavascript.com/improving-on-javascripts-random-numbers/		
 	if (min && max) {
