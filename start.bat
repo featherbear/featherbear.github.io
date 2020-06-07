bd6ee9fa3888b672f16b48db54e0cd2be1f412c1
Andrew Wong <andrew.j.wong@outlook.com>
2020-06-07 18:11:48 +1000

    ♪ It's the start, of version 3. It Sveltes so right, do be done with Vue - oh oh ♪

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title featherbear.au.eu.org developmental server
-start http://localhost:8080
-http-server -p 8080
-; npm install http-server -g
-python -m SimpleHTTPServer 8080
-python -m http.server 8080
