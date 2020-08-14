3ff81939bcd4f8e9872c9752d6525f1e0527f4db
Andrew Wong <andrew.j.wong@outlook.com>
2020-08-14 21:26:42 +1000

    Add projects as JSON
    Install json-parser

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import replace from '@rollup/plugin-replace'
 import commonjs from '@rollup/plugin-commonjs'
 import svelte from 'rollup-plugin-svelte'
 import babel from '@rollup/plugin-babel'
+import json from '@rollup/plugin-json'
 import { terser } from 'rollup-plugin-terser'
 import config from 'sapper/config/rollup.js'
 import sveltePreprocess from 'svelte-preprocess'
@@ -44,6 +45,7 @@ export default {
         'process.browser': true,
         'process.env.NODE_ENV': JSON.stringify(mode)
       }),
+      json({ compact: true }),
       svelte({
         dev,
         hydratable: true,
@@ -98,6 +100,7 @@ export default {
         'process.browser': false,
         'process.env.NODE_ENV': JSON.stringify(mode)
       }),
+      json({ compact: true }),
       svelte({
         generate: 'ssr',
         dev,
@@ -107,6 +110,7 @@ export default {
         dedupe: ['svelte']
       }),
       commonjs()
+
     ],
     external: Object.keys(pkg.dependencies).concat(
       require('module').builtinModules ||
