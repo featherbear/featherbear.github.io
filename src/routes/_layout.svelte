e71a8d2e7069bac2d8af27c3e09923af16d83369
Andrew Wong <andrew.j.wong@outlook.com>
2020-08-14 21:29:58 +1000

    Add mobile CSS for responsive

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -8,6 +8,13 @@
   @import "bulma/sass/utilities/_all.sass";
   @import "bulma/sass/helpers/_all.sass";
   @import "bulma/sass/elements/container.sass";
+
+  .container {
+    @media screen and (max-width: 1024px) {
+      padding-left: 0.75rem;
+      padding-right: 0.75rem;
+    }
+  }
 </style>
 
 {#if segment !== undefined}
