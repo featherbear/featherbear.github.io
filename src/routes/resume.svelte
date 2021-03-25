2a6e854d1eddefb3db36d2d343bc1fe828bb948b
Andrew Wong <andrew.j.wong@outlook.com>
2021-03-26 04:15:32 +1100

    Begin Timeline component
    Add line and dot - aligned to their centres with SCSS

--- src/routes/resume.svelte
+++ src/routes/resume.svelte
@@ -1,5 +1,7 @@
 <script>
   import ProfileContainer from "../components/resume/ProfileContainer/ProfileContainer.svelte";
+  import Timeline from '../components/resume/Timeline/'
 </script>
 
-<ProfileContainer />
+<!-- <ProfileContainer /> -->
+<Timeline />
