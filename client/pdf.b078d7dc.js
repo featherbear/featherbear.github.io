4acdbb5d041b1a5c6517f02275ab6feb540057e4
featherbear <featherbear@users.noreply.github.com>
2021-09-07 11:07:38 +0000

    Automated deployment: Tue Sep  7 11:07:38 UTC 2021 12612e95ae6de299350a12e92cd74060aa11d1fc

--- client/pdf.b078d7dc.js
+++
@@ -1,1 +0,0 @@
-import{S as e,i as s,s as t}from"./client.91c8f247.js";async function n(e,s){return this.redirect(301,"Andrew Jin-Meng Wong - Resume (Complete).pdf")}export default class extends e{constructor(e){super(),s(this,e,null,null,t,{})}}export{n as preload};
