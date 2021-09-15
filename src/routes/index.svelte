c361d674b50a3a46013c3a489ff8df846bbb067c
Andrew Wong <andrew.j.wong@outlook.com>
2021-09-16 07:53:59 +1000

    Set A/B

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,29 @@
 <script>
+  import { onMount } from "svelte";
+
+  import DesignA from "../components/home/designA/_DesignA.svelte";
   import DesignB from "../components/home/designB/_DesignB.svelte";
+
+  let page;
+  onMount(() => {
+    let usePageB = Math.random() >= 0.5;
+    page = usePageB ? DesignB : DesignA;
+    if (
+      process.browser &&
+      process.env.NODE_ENV !== "development" &&
+      location.hostname !== "localhost"
+    ) {
+      window.fathom("trackPageview", {
+        path: "#abtest-" + (usePageB ? "B" : "A"),
+      });
+    }
+  });
 </script>
 
 <svelte:head>
   <title>Andrew Wong | featherbear</title>
 </svelte:head>
 
-<DesignB />
+{#if page}
+  <svelte:component this={page} />
+{/if}
