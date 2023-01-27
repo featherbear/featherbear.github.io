39ac152666d56b2f8570f25d66d819a03fa7eda5
Andrew Wong <andrew.j.wong@outlook.com>
2023-01-28 03:23:27 +1100

    Update aboutText.js

--- src/data/aboutText.js
+++ src/data/aboutText.js
@@ -1,4 +1,2 @@
-export default `Code monkey, hardware enthusiast, photographer and wannabe musician.  
-Also really interested in audio/visual production, flashing lights and all that bass 🔊
-
-Currently a Computer Engineering student at the University of New South Wales.`
+export default `Hardware hacker by day, a different sort of hacker by night...
+Software, Multimedia, AV / Event Production`
